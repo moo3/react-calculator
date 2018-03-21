ac52da0ad153dc829865ff5e06b4a4b15b59e9a2
Moorthy Venkatraman <moorthy.sv@gmail.com>
2018-03-20 22:01:43 -0400

    Base code. WIP

---
+++ src/Calculator.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './Calculator.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Calculator</h1>
+        </header>
+        <section>
+          In progress...
+        </section>
+      </div>
+    );
+  }
+}
+
+export default App;
