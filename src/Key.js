79df789f22ca08bd1f9f3aef862c18ad1c69ea62
Moorthy Venkatraman <moorthy.sv@gmail.com>
2018-03-25 23:41:17 -0400

    a bit of styling the keypad

--- src/Key.js
+++ src/Key.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 
 class Key extends Component {
+    keyStyle = {
+        'backgroundColor': '#4CAF50', /* Green */
+        border: 'none',
+        color: 'white',
+        padding: '30px',
+        margin: '10px'
+    };
+
     handleClick = () => {
         this.props.onClick(this.props.code);
     }
 
     render() {
         return (
-            <button onClick={this.handleClick}>
+            <button onClick={this.handleClick} style={this.keyStyle}>
                 {this.props.code}
             </button>
         );
